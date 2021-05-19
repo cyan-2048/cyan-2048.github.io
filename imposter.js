e393f8b0f0a2e10034279dbee533aa3547976cc6
Cyanuu Reeves <72785466+cyan-2048@users.noreply.github.com>
2021-05-20 05:15:35 +0800

    Add files via upload

---
+++ imposter.js
@@ -0,0 +1,31 @@
+const darkmode = new darken({
+      toggle: "#toggle",
+      variables : {
+        "--primary-color" : ["#ffffff", "#1f1f1f"],
+        "--secondary-color" : ["#000000", "#ffffff"],
+      }
+});
+
+
+function notrickroll() {
+  window.open('https://www.youtube.com/watch?v=Lrj2Hq7xqQ8', '_blank');
+}
+
+
+Array.from(document.getElementsByClassName("dogey")).forEach(function(element) {
+  element.addEventListener('click', douge);
+});
+
+function douge() {
+  document.getElementById('ursus').value += this.getAttribute('stopbeingsus');
+}
+
+
+function sussybaka() {
+  if (document.getElementById('ursus').value == 'why tf are you still here'){
+    window.open('https://www.youtube.com/watch?v=Lrj2Hq7xqQ8', '_blank');
+    document.getElementById('ursus').value = '';
+  } else {
+    document.getElementById('ursus').value = ''
+  }
+}
