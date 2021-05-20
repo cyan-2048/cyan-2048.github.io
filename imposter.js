f9f7d7b968ba0870b1c6325048b9758381ec21c5
Cyanuu Reeves <72785466+cyan-2048@users.noreply.github.com>
2021-05-21 01:56:37 +0800

    a few more....

--- imposter.js
+++ imposter.js
@@ -25,10 +25,30 @@ function sussybaka() {
   if (document.getElementById('ursus').value == 'arebeingpleaseyou'){
     window.open('https://www.youtube.com/watch?v=Lrj2Hq7xqQ8', '_blank');
     document.getElementById('ursus').value = '';
-  } else {
+  }
+  if (document.getElementById('ursus').value == 'susjustGoaway'){
+    groovity();
+    document.getElementById('ursus').value = '';
+  }
+  if (document.getElementById('ursus').value == 'dogedogedogeyouarebeingsusushouldgoawaypleasejustGostOpreadingthisstopit'){
+    window.location.href = 'https://cyan2048.web.fc2.com/bonk-it.jpg';
+    document.getElementById('ursus').value = '';
+  }
+  else {
     document.getElementById('ursus').value = ''
   }
 }
+
+/*
+if (document.getElementById('ursus').value == ''){
+
+  document.getElementById('ursus').value = '';
+}
+*/
+
+
+
+
 
 Array.from(document.getElementsByClassName("dоgeyy")).forEach(function(element) {
   element.addEventListener('click', sussybaka);
@@ -37,3 +57,5 @@ Array.from(document.getElementsByClassName("dоgeyy")).forEach(function(element)
 Array.from(document.getElementsByClassName("tittle")).forEach(function(element) {
   element.addEventListener('click', notrickroll);
 });
+
+function groovity(){var t,e,o,l;for(t=document.querySelectorAll(".dogeyy"),e=0;e<t.length;e++)t[e].style.width="auto",t[e].style.height="150px";for(o=document.querySelectorAll(".dоgeyy"),l=0;l<o.length;l++)o[l].style.width="auto",o[l].style.height="150px";var r=document.createElement("script");r.src="https://cyan2048.web.fc2.com/gravity-autorun.js",document.body.appendChild(r)}
