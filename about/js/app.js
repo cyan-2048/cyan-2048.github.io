ab0209469bf4103390fd40705a260c7f1b9a9c05
Cyan <cyankindasus@protonmail.com>
2022-01-03 23:45:55 +0800

    update, maybe support for touchscreen i guess

--- about/js/app.js
+++ about/js/app.js
@@ -1,13 +1,6 @@
 var window_styles = new Object();
 var start_open = false;
 
-$('.window#traits').resizable({
-	handles: 'all',
-	containment: 'body',
-	minWidth: 400,
-	minHeight: 317,
-});
-
 var tabs = [];
 
 function updateTabs() {
@@ -22,25 +15,13 @@ setTimeout(updateTabs, 500);
 function runApp(g) {
 	switch (g) {
 		case 'minecraft':
-			$('body').append(
-				`<div class="window glass" id="minecraft" style="left:346px;width:674px;top:61px;height:474px;z-index:${
-					highestZ() + 1
-				}"><div class="title-bar"><img src="css/vscode_iframe/crafting.png" style="width:18px;height:18px;position:absolute;left:6px"><div class="title-bar-text">Minecraft</div><div class="title-bar-controls"><button aria-label="Minimize"></button> <button onclick="maxsBtn(this)" aria-label="Maximize"></button> <button aria-label="Close" onclick="closBtn(this)"></button></div></div><div class="window-body"><iframe src="https://classic.minecraft.net"></iframe></div></div>`
-			);
+			$('body').append(`<div class="window glass" id="minecraft" style="left:346px;width:674px;top:61px;height:474px;z-index:${highestZ() + 1}"><div class="title-bar"><img src="css/vscode_iframe/crafting.png" style="width:18px;height:18px;position:absolute;left:6px"><div class="title-bar-text">Minecraft</div><div class="title-bar-controls"><button aria-label="Minimize"></button> <button onclick="maxsBtn(this)" aria-label="Maximize"></button> <button aria-label="Close" onclick="closBtn(this)"></button></div></div><div class="window-body"><iframe src="https://classic.minecraft.net"></iframe></div></div>`);
 			break;
 		case 'vscode':
-			$('body').append(
-				`<div class="window" id="vscode" style="left:346px;width:674px;top:61px;height:474px;z-index:${
-					highestZ() + 1
-				}"><div class="title-bar"><div class="vscode_icon"></div><div class="title-bar-text">Visual Studio Code</div><div class="title-bar-controls"><button aria-label="Minimize"></button> <button onclick="maxsBtn(this)" aria-label="Maximize"></button> <button aria-label="Close" onclick="closBtn(this)"></button></div></div><iframe src="https://vscode.dev/"></iframe></div>`
-			);
+			$('body').append(`<div class="window" id="vscode" style="left:346px;width:674px;top:61px;height:474px;z-index:${highestZ() + 1}"><div class="title-bar"><div class="vscode_icon"></div><div class="title-bar-text">Visual Studio Code</div><div class="title-bar-controls"><button aria-label="Minimize"></button> <button onclick="maxsBtn(this)" aria-label="Maximize"></button> <button aria-label="Close" onclick="closBtn(this)"></button></div></div><iframe src="https://vscode.dev/"></iframe></div>`);
 			break;
 		case 'traits':
-			$('body').append(
-				`<div class="window glass" id=traits style=left:723px;width:400px;top:87px;height:317px;z-index:${
-					highestZ() + 1
-				}><div class=title-bar><div class=title-bar-text>Another window with contents</div><div class=title-bar-controls><button aria-label=Minimize></button> <button onclick=maxsBtn(this) aria-label=Maximize></button> <button aria-label=Close onclick=closBtn(this)></button></div></div><div class=window-body><menu role=tablist><button role=tab aria-controls=music aria-selected=true>Music</button> <button role=tab aria-controls=dogs>Dogs</button> <button role=tab aria-controls=food>Food</button></menu><article role=tabpanel id=music><p>Set your listening preferences</p><fieldset><legend>Today's mood</legend><div class=field-row><input id=radio25 type=radio name=fieldset-example2> <label for=radio25>Nicki Minaj</label></div><div class=field-row><input id=radio26 type=radio name=fieldset-example2> <label for=radio26>Bell Towers</label></div><div class=field-row><input id=radio27 type=radio name=fieldset-example2> <label for=radio27>The Glamorous Monique</label></div><div class=field-row><input id=radio28 type=radio name=fieldset-example2> <label for=radio28>EN. V</label></div></fieldset><section class=field-row><button>Reset Alarm...</button> <label>Try this to get some attention</label></section></article><article role=tabpanel hidden id=dogs><img style=width:100% src=css/dog.jpg></article><article role=tabpanel hidden id=food><p>You create the content for each tab by using an <code>article</code> tag.</p></article><section class=field-row style=justify-content:flex-end><button>OK</button> <button>Cancel</button></section></div></div>`
-			);
+			$('body').append(`<div class="window glass" id=traits style=left:723px;width:400px;top:87px;height:317px;z-index:${highestZ() + 1}><div class=title-bar><div class=title-bar-text>Another window with contents</div><div class=title-bar-controls><button aria-label=Minimize></button> <button onclick=maxsBtn(this) aria-label=Maximize></button> <button aria-label=Close onclick=closBtn(this)></button></div></div><div class=window-body><menu role=tablist><button role=tab aria-controls=music aria-selected=true>Music</button> <button role=tab aria-controls=dogs>Dogs</button> <button role=tab aria-controls=food>Food</button></menu><article role=tabpanel id=music><p>Set your listening preferences</p><fieldset><legend>Today's mood</legend><div class=field-row><input id=radio25 type=radio name=fieldset-example2> <label for=radio25>Nicki Minaj</label></div><div class=field-row><input id=radio26 type=radio name=fieldset-example2> <label for=radio26>Bell Towers</label></div><div class=field-row><input id=radio27 type=radio name=fieldset-example2> <label for=radio27>The Glamorous Monique</label></div><div class=field-row><input id=radio28 type=radio name=fieldset-example2> <label for=radio28>EN. V</label></div></fieldset><section class=field-row><button>Reset Alarm...</button> <label>Try this to get some attention</label></section></article><article role=tabpanel hidden id=dogs><img style=width:100% src=css/dog.jpg></article><article role=tabpanel hidden id=food><p>You create the content for each tab by using an <code>article</code> tag.</p></article><section class=field-row style=justify-content:flex-end><button>OK</button> <button>Cancel</button></section></div></div>`);
 			makeTabsWorkAgain();
 			break;
 	}
@@ -50,10 +31,11 @@ function runApp(g) {
 	updateTabs();
 }
 
+var aeroInterval = null;
+
 function makeWindowsDrag() {
 	$('.window').draggable({
 		handle: '.title-bar',
-		containment: 'body',
 		stack: '.window',
 		start: (e) => {
 			if (e.target.classList.contains('maximu')) {
@@ -70,27 +52,67 @@ function makeWindowsDrag() {
 			for (let a of document.querySelectorAll('.title-bar-controls')) {
 				a.classList.add('notfocused');
 			}
-			e.target
-				.querySelector('.title-bar-controls')
-				.classList.remove('notfocused');
+
+			e.target.querySelector('.title-bar-controls').classList.remove('notfocused');
 		},
 		drag: (e) => {
-			if (
-				e.target.style.left ==
-				window.innerWidth - e.target.offsetWidth + 'px'
-			) {
-				console.log('edge edge edge edge');
+			var faildow = e.target;
+			if (window.innerWidth < Number(faildow.style.left.split('px')[0]) + faildow.offsetWidth && +e.clientX > window.innerWidth - 1 && faildow.classList.contains('ui-resizable')) {
+				console.log('right edge edge edge edge ');
 			}
-			if (e.target.style.left == '0px') {
-				console.log('0 edge edge edge edge');
+			if (Number(faildow.style.left.split('px')[0]) < 0 && e.clientX < 1 && faildow.classList.contains('ui-resizable')) {
+				console.log('left 0 edge edge edge edge ');
 				//	return false;
 			}
-			if (e.target.style.top == '0px') {
-				console.log('0 top top edge edge');
+			if (Number(faildow.style.top.split('px')[0]) < 0 && e.clientY < 1 && faildow.classList.contains('ui-resizable')) {
+				console.log('0 top top edge edge ');
 			}
 		},
-		stop: (e) => {},
+		stop: (e) => {
+			for (let fail of $('.window')) {
+				var top = Number(fail.style.top.split('px')[0]);
+				if (top < 0) {
+					fail.style.top = '0px';
+				} else if (top > window.innerHeight - 67) {
+					fail.style.top = window.innerHeight - 67 + 'px';
+				}
+			}
+		},
 	});
+
+	for (let object of document.querySelectorAll('.window')) {
+		var initX, initY, firstX, firstY;
+
+		object.addEventListener(
+			'touchstart',
+			function (e) {
+				e.preventDefault();
+				initX = this.offsetLeft;
+				initY = this.offsetTop;
+				var touch = e.touches;
+				firstX = touch[0].pageX;
+				firstY = touch[0].pageY;
+
+				this.addEventListener('touchmove', swipeIt, false);
+
+				window.addEventListener(
+					'touchend',
+					function (e) {
+						e.preventDefault();
+						object.removeEventListener('touchmove', swipeIt, false);
+					},
+					false
+				);
+			},
+			false
+		);
+
+		function swipeIt(e) {
+			var contact = e.touches;
+			this.style.left = initX + contact[0].pageX - firstX + 'px';
+			this.style.top = initY + contact[0].pageY - firstY + 'px';
+		}
+	}
 
 	$('.window').on('click', (e) => {
 		console.log(e);
@@ -99,9 +121,7 @@ function makeWindowsDrag() {
 		for (let a of document.querySelectorAll('.title-bar-controls')) {
 			a.classList.add('notfocused');
 		}
-		e.delegateTarget
-			.querySelector('.title-bar-controls')
-			.classList.remove('notfocused');
+		e.delegateTarget.querySelector('.title-bar-controls').classList.remove('notfocused');
 	});
 
 	$('.window#vscode, .window#minecraft').resizable({
@@ -109,6 +129,19 @@ function makeWindowsDrag() {
 		containment: 'body',
 		minWidth: 576,
 		minHeight: 380,
+	});
+
+	$('.window#traits').resizable({
+		handles: 'all',
+		containment: 'body',
+		minWidth: 400,
+		minHeight: 317,
+		start: (e) => {
+			if (e.target.classList.contains('maximu')) {
+				e.target.classList.remove('maximu');
+				e.target.querySelector('[aria-label="Restore"]').outerHTML = '<button onclick="maxsBtn(this)" aria-label="Maximize"></button>';
+			}
+		},
 	});
 }
 
@@ -131,8 +164,7 @@ function maxsBtn(e) {
 		a.style = `top: -2px; left: -1px; width: 100.4vw; height: calc(100vh - 42px); z-index: ${a.style.zIndex};`;
 	}
 	setTimeout(() => {
-		e.outerHTML =
-			'<button onclick="resBtn(this)" aria-label="Restore"></button>';
+		e.outerHTML = '<button onclick="resBtn(this)" aria-label="Restore"></button>';
 	}, 100);
 }
 function closBtn(e) {
@@ -149,8 +181,7 @@ $(document).on('focusout', function () {
 		// using the 'setTimout' to let the event pass the run loop
 		if (document.activeElement instanceof HTMLIFrameElement) {
 			// Do your logic here..
-			$(document.activeElement).closest('.window')[0].style['z-index'] =
-				highestZ() + 1;
+			$(document.activeElement).closest('.window')[0].style['z-index'] = highestZ() + 1;
 			for (let a of document.querySelectorAll('.title-bar-controls')) {
 				a.classList.add('notfocused');
 			}
@@ -166,8 +197,7 @@ function resBtn(e) {
 		console.log(window_styles[b]);
 		a.setAttribute('style', window_styles[b]);
 		setTimeout(() => {
-			e.outerHTML =
-				'<button onclick="maxsBtn(this)" aria-label="Maximize"></button>';
+			e.outerHTML = '<button onclick="maxsBtn(this)" aria-label="Maximize"></button>';
 		}, 100);
 	}
 }
@@ -180,18 +210,10 @@ function makeTabsWorkAgain() {
 			e.preventDefault();
 			const tabContainer = e.target.parentElement.parentElement.parentElement;
 			const targetId = e.target.getAttribute('aria-controls');
-			tabContainer
-				.querySelectorAll("button[role='tab']")
-				.forEach((_tabButton) =>
-					_tabButton.setAttribute('aria-selected', false)
-				);
+			tabContainer.querySelectorAll("button[role='tab']").forEach((_tabButton) => _tabButton.setAttribute('aria-selected', false));
 			tabButton.setAttribute('aria-selected', true);
-			tabContainer
-				.querySelectorAll('[role=tabpanel]')
-				.forEach((tabPanel) => tabPanel.setAttribute('hidden', true));
-			tabContainer
-				.querySelector(`[role=tabpanel]#${targetId}`)
-				.removeAttribute('hidden');
+			tabContainer.querySelectorAll('[role=tabpanel]').forEach((tabPanel) => tabPanel.setAttribute('hidden', true));
+			tabContainer.querySelector(`[role=tabpanel]#${targetId}`).removeAttribute('hidden');
 		});
 	});
 }
@@ -213,13 +235,7 @@ updateTime = () => {
 		hour = 12;
 	}
 	var time = hour + ':' + minutes + ' ' + pam;
-	var date =
-		datetime.getMonth() +
-		1 +
-		'/' +
-		datetime.getDate() +
-		'/' +
-		datetime.getFullYear();
+	var date = datetime.getMonth() + 1 + '/' + datetime.getDate() + '/' + datetime.getFullYear();
 	$('#datetime > .time').text(time);
 	$('#datetime > .date').text(date);
 };
@@ -245,10 +261,7 @@ for (let a of document.querySelectorAll('.title-bar-controls')) {
 }
 
 if (!CSS.supports('backdrop-filter', 'blur(15px)')) {
-	document.body.style.setProperty(
-		'--backdrop',
-		'url("marble_blur.jpeg") 0 / cover fixed'
-	);
+	document.body.style.setProperty('--backdrop', 'url("marble_blur.jpeg") 0 / cover fixed');
 	document.querySelector('#backdrop-not').style.opacity = '1';
 } else {
 	$('#backdrop-not').remove();
@@ -270,22 +283,15 @@ $('body').on('dblclick', function (e) {
 	toggleFullscreen();
 });
 
-$('.title-bar').on('dblclick', function (e) {
-	e.delegateTarget
-		.querySelector("[aria-label='Maximize'],[aria-label='Restore']")
-		.click();
+$('.window.ui-resizable .title-bar').on('dblclick', function (e) {
+	e.delegateTarget.querySelector("[aria-label='Maximize'],[aria-label='Restore']").click();
 });
 
-document
-	.querySelector('#start_input')
-	.addEventListener('keypress', function (e) {
-		if (e.key === 'Enter') {
-			window.open(
-				`http://www.google.com/search?q=${encodeURIComponent(e.target.value)}`,
-				'_blank'
-			);
-		}
-	});
+document.querySelector('#start_input').addEventListener('keypress', function (e) {
+	if (e.key === 'Enter') {
+		window.open(`http://www.google.com/search?q=${encodeURIComponent(e.target.value)}`, '_blank');
+	}
+});
 
 function highestZ() {
 	var array = [];
@@ -324,17 +330,12 @@ function toggleStart() {
 		setTimeout(() => {
 			document.getElementById('start_menu').style.opacity = '1';
 		}, 10);
-		document.getElementById('menu-button').style.backgroundImage =
-			'url(css/start-selected.png)';
+		document.getElementById('menu-button').style.backgroundImage = 'url(css/start-selected.png)';
 		document.getElementById('start_input').focus();
 		document.getElementById('start_input').addEventListener('blur', whenBlur);
 	} else {
-		document
-			.getElementById('start_input')
-			.removeEventListener('blur', whenBlur);
+		document.getElementById('start_input').removeEventListener('blur', whenBlur);
 		document.getElementById('menu-button').setAttribute('style', '');
-		document
-			.getElementById('start_menu')
-			.setAttribute('style', 'display: none;');
+		document.getElementById('start_menu').setAttribute('style', 'display: none;');
 	}
 }
