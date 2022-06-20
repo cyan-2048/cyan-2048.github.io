d0f997d1dfba86e945d1b1a47486fcf323098863
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2022-06-20 08:44:21 +0800

    migrate to cloudflare

--- about/js/socials.js
+++ about/js/socials.js
@@ -1,15 +1,10 @@
 ((window) => {
 	window.socials = {};
 	function loop() {
-		fetch("https://cyan-socials.herokuapp.com/")
+		fetch("https://website.cyan-2048.workers.dev/socials")
 			.then((e) => e.json())
 			.then((e) => {
 				window.socials = e;
-				if (window.socials.telegram) {
-					fetch(window.socials.telegram.avatar)
-						.then((b) => b.blob())
-						.then((b) => (window.socials.telegram.avatar = URL.createObjectURL(b)));
-				}
 			})
 			.catch(loop);
 	}
